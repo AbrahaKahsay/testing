f57f63793c6f37d58da747206831eb5409712790
Abraha Kahsay <abrahak81@gmail.com>
2022-07-12 18:56:22 +0300

    Modify string test function to throw errors

--- tests/stringlength.test.js
+++ tests/stringlength.test.js
@@ -2,4 +2,5 @@ const stringLength = require('../jsmodules/stringlength');
 
 test('counts string character', () => {
   expect(stringLength('hello')).toBe(5);
+  expect(stringLength('this will generate error').toBeFalsy());
 });
