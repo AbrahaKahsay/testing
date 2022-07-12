9ac1e3228146ed215e03aae28167701cda6f7343
Abraha Kahsay <abrahak81@gmail.com>
2022-07-12 18:52:05 +0300

    Modify string function to throw errors

--- jsmodules/stringlength.js
+++ jsmodules/stringlength.js
@@ -3,6 +3,9 @@ const stringLength = (string) => {
     if(string.length<1 || string.length>10){
         return "Errors";
     }
-    return string.length;
+    else 
+    {
+        string.length;
+    }
 }
 module.exports = stringLength;
