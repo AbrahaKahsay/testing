587b7395729713954f32d8d8dd40794e16e44b8f
Abraha Kahsay <abrahak81@gmail.com>
2022-07-12 19:03:47 +0300

    Remove parenthesis from expect syntax

--- jsmodules/stringlength.js
+++ jsmodules/stringlength.js
@@ -1,11 +1,11 @@
 // Task 1
 const stringLength = (string) => {
     if(string.length<1 || string.length>10){
-        return "Errors";
+        return false;
     }
     else 
     {
-        string.length;
+    return string.length;
     }
 }
 module.exports = stringLength;
